82f6e1072056e0e80a9dce338efbc08515a77d2b
juan Carlos Romero <A01658390@tec.mx>
2024-03-22 12:56:45 -0600

    Se realizaron los comentarios requeridos

--- cannon.py
+++ cannon.py
@@ -10,31 +10,27 @@ Exercises
 
 from random import randrange
 from turtle import *
-
 from freegames import vector
 
 ball = vector(-200, -200)
 speed = vector(0, 0)
 targets = []
 
-
+#Se camiaron los números que correspondian a la velocidad para aumentarla en el juego
 def tap(x, y):
-    """Respond to screen tap."""
+    "Respond to screen tap."
     if not inside(ball):
         ball.x = -199
         ball.y = -199
-       
-        speed.x = (x + 200) / 10
-        speed.y = (y + 200) / 10
-
+        speed.x = (x + 200) / 25
+        speed.y = (y + 200) / 25
 
 def inside(xy):
-    """Return True if xy within screen."""
+    "Return True if xy within screen."
     return -200 < xy.x < 200 and -200 < xy.y < 200
 
-
 def draw():
-    """Draw ball and targets."""
+    "Draw ball and targets."
     clear()
 
     for target in targets:
@@ -47,21 +43,18 @@ def draw():
 
     update()
 
-
 def move():
-    """Move ball and targets."""
+    "Move ball and targets."
     if randrange(40) == 0:
         y = randrange(-150, 150)
         target = vector(200, y)
         targets.append(target)
 
     for target in targets:
-     
-        target.x -= 2
+        target.x -= 0.5
 
     if inside(ball):
-        
-        speed.y -= 0.7
+        speed.y -= 0.35
         ball.move(speed)
 
     dupe = targets.copy()
@@ -71,14 +64,18 @@ def move():
         if abs(target - ball) > 13:
             targets.append(target)
 
-    draw()
-
+#Se cambiaron las posiciones iniciales 
     for target in targets:
         if not inside(target):
-            return
+            targets.remove(target)
+            x = 200  # Nueva posición x (fuera de la pantalla)
+            y = randrange(-150, 150)  # Nueva posición y (aleatoria)
+            target.x = x
+            target.y = y
 
-    ontimer(move, 25)  
+    draw()
 
+    ontimer(move, 50)
 
 setup(420, 420, 370, 0)
 hideturtle()
@@ -87,4 +84,3 @@ tracer(False)
 onscreenclick(tap)
 move()
 done()
-
